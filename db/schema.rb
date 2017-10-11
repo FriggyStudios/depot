e5d7319334d7b2089b1a8f60cb363a81e542c9eb
Kevin <k.mcginley@nuigalway.ie>
2017-10-11 11:11:32 +0100

    cart

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920103711) do
+ActiveRecord::Schema.define(version: 20171011100835) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
