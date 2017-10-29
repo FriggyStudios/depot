4461956cb233cec9aa21dc9320dd1a2f78e4c6f2
Kevin <k.mcginley@nuigalway.ie>
2017-10-29 18:36:31 +0000

    Ajax add to cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,8 +30,9 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart, notice: 'Product added to cart.' }
-        format.json { render :show, status: :created, location: @line_item }
+       format.html { redirect_to store_url }
+       format.json { render :show, status: :created, location: @line_item }
+       format.js
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
