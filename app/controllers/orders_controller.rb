70a2248e2b0a516ef3d9af461fd49f14fc8b60ed
DoM04 <donalom1@gmail.com>
2017-11-15 11:55:57 +0000

    push

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -31,7 +31,7 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
-    @order.add_line_items_from_cart(@cart)
+    #@order.add_line_items_from_cart(@cart)
     respond_to do |format|
       if @order.save
         Cart.destroy(session[:cart_id])
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,4 @@
 class Order < ActiveRecord::Base
-has_many :line_items, dependent: :destroy
   PAYMENT_TYPES = [ "Cheque", "Credit card", "Purchase order" ]
   
   def add_line_items_from_cart(cart)
